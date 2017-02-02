a8b1c247bfff95a5372e026974631e666742c6b4
Matthew Ell <matt.ell@alleyinteractive.com>
2017-02-02 16:45:59 -0500

    initial build and update header to include webpack compiled assets

---
+++ public/static/js/global.bundle.js
@@ -0,0 +1,1 @@
+!function(o){function r(e){if(t[e])return t[e].exports;var n=t[e]={exports:{},id:e,loaded:!1};return o[e].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var t={};return r.m=o,r.c=t,r.p="",r(0)}([function(o,r,t){t(1),o.exports=t(2)},function(o,r){"use strict";var t="hello webpack";console.log(t)},function(o,r){}]);
