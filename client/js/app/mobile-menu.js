f28f70c0fc7e6972992be69500f2361b11b0497d
Matt N <mattn@alleyinteractive.com>
2017-02-03 17:48:06 -0600

    initial mobile menu js

---
+++ client/js/app/mobile-menu.js
@@ -0,0 +1,18 @@
+export default function () {
+  const menuBtn = document.querySelector('.hamburger-menu');
+  const mainNav = document.querySelector('.main-nav');
+  const menuClose = document.querySelector('.mobile-close');
+  if (menuBtn) {
+    menuBtn.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      mainNav.classList.add('active');
+    });
+  }
+  if (menuClose) {
+    menuClose.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      mainNav.classList.remove('active');
+    });
+  }
+  console.log('hellhhho');
+}
