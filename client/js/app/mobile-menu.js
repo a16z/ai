8f0151f520073e7a8bdb2c78eb49c6f0b0f23d8c
Matt N <mattn@alleyinteractive.com>
2017-02-08 14:25:33 -0600

    mobile menu close js

--- client/js/app/mobile-menu.js
+++ client/js/app/mobile-menu.js
@@ -1,11 +1,17 @@
 export default function () {
   const menuBtn = document.getElementById('icon-bars');
   const sideBar = document.getElementById('sidebar');
+  const menuClose = document.getElementById('close-menu');
   if (menuBtn) {
     menuBtn.addEventListener('click', (evt) => {
       evt.preventDefault();
       sideBar.classList.add('active');
     });
   }
-  console.log(menuBtn);
+  if (menuClose) {
+    menuClose.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      sideBar.classList.remove('active');
+    });
+  }
 }
